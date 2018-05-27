8fa077e2d90f38c245b15a86eaa0e9d86d80d16a
richcow2018 <39473760+richcow2018@users.noreply.github.com>
2018-05-27 16:32:51 +0800

    Initial project
    Initial project with config:
    ASP.NET Core Web application (MVC) by C#

---
+++ RichCow/RichCow/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace RichCow.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
